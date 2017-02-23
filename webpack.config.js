49aa19ff19cbd5a5e7700108f787165fcf0484fa
dsky1990 <dutian1990@gmail.com>
2017-02-23 10:57:44 +0800

    add filePath

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 /**
  * Created by dsky on 2016/12/22.
  */
+
+// get filePath
 const fs = require('fs');
 const fileStr = (fs.readFileSync('./entry-file.js', 'utf8')).replace(/\s+/g,"").replace(';','');
 const filePath = JSON.parse(fileStr.substring(fileStr.indexOf('=')+1));
-console.log("buildSass", filePath.buildSass);
+const buildSass = filePath.buildSass;
+const buildCss = filePath.buildCss;
+const buildEs6 = filePath.buildEs6;
+const buildJs = filePath.buildJs;
+const buildIcon = filePath.buildIcon;
+const buildImg = filePath.buildImg;
+const distCss = filePath.distCss;
+const distImg = filePath.distImg;
+const distJs = filePath.distJs;
 
