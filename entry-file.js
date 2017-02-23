8d280591ab09b94ffca1eb3d5045c4fd7ed0024e
dsky1990 <dutian1990@gmail.com>
2017-02-23 10:54:30 +0800

    get filePath from entry-file.js

--- entry-file.js
+++ entry-file.js
@@ -1,14 +1,11 @@
-/**
- * Created by dsky on 2016/12/22.
- */
-var path = {
-    buildSass: './build/sass',
-    buildCss: './build/css',
-    buildEs6: './build/es6',
-    buildJs: './build/js',
-    buildIcon: './build/icon',
-    buildImg: './build/img',
-    distCss: './dist/css',
-    distImg: './dist/img',
-    distJs: './dist/js'
+const path = {
+    "buildSass": "./build/sass",
+    "buildCss": "./build/css",
+    "buildEs6": "./build/es6",
+    "buildJs": "./build/js",
+    "buildIcon": "./build/icon",
+    "buildImg": "./build/img",
+    "distCss": "./dist/css",
+    "distImg": "./dist/img",
+    "distJs": "./dist/js"
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
 /**
  * Created by dsky on 2016/12/22.
  */
+const fs = require('fs');
+const fileStr = (fs.readFileSync('./entry-file.js', 'utf8')).replace(/\s+/g,"").replace(';','');
+const filePath = JSON.parse(fileStr.substring(fileStr.indexOf('=')+1));
+console.log("buildSass", filePath.buildSass);
+
