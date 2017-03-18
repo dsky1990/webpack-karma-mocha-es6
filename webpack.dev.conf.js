58b4a21868759c878af38017d7c48ea5f7498709
dsky1990 <dutian1990@gmail.com>
2017-03-18 16:55:48 +0800

    add base,dev,prod conf

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,3 +1,60 @@
 /**
  * Created by dsky on 2017/3/17.
  */
+// development config
+module.exports = {
+    entry: {
+        index: '',
+        vendor: ''
+    },
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.reslove(_dirname, build)
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: 'css-loader'
+            })
+        },{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [['es2015', {modules: false}]],
+                    plugins: [
+                        'syntax-dynamic-import',
+                        'transform-async-to-generator',
+                        'transform-regenerator',
+                        'transform-runtime'
+                    ]
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest'],
+            minChunks: Infinity,
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new WebpackChunkHash(),
+        new ChunkManifestPlugin({
+            filename: "chunk-manifest.json",
+            manifestVariable: "webpackManifest"
+        }),
+        function() {
+            this.plugin("done", function(stats) {
+                fs.writeFileSync(
+                    path.join(__dirname, build, "stats.json"),
+                    JSON.stringify(stats.toJson()));
+            });
+        }
+    ]
+};
