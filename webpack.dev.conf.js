b46b2c877e58ec33cd9e9ba23a32e19a5d9267da
dsky1990 <dutian1990@gmail.com>
2017-03-20 20:32:08 +0800

    update dev config

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,60 +1,71 @@
 /**
  * Created by dsky on 2017/3/17.
  */
-// development config
-module.exports = {
-    entry: {
-        index: '',
-        vendor: ''
-    },
-    output: {
-        filename: '[name].[chunkhash].js',
-        path: path.reslove(_dirname, build)
-    },
+/*
+* development config
+* vue to js
+* eslint
+* sass to css
+* es6 to js
+* stylelint
+*/
+const webpackMerge = require('webpack-merge');
+const baseConf = require('./webpack.base.conf');
+module.exports = webpackMerge(baseConf, {
+    devtool: '#cheap-module-eval-source-map',
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                use: 'css-loader'
-            })
-        },{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: [['es2015', {modules: false}]],
-                    plugins: [
-                        'syntax-dynamic-import',
-                        'transform-async-to-generator',
-                        'transform-regenerator',
-                        'transform-runtime'
-                    ]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            },
+            {
+                enforce: "pre",
+                test: /\.(js|vue)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "eslint-loader",
+                    options: {
+                        cache : true,
+                        quiet: true,
+                        failOnError: true
+                    }
                 }
-            }]
-        }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['es2015', {modules: false}]],
+                        plugins: [
+                            'syntax-dynamic-import',
+                            'transform-async-to-generator',
+                            'transform-regenerator',
+                            'transform-runtime'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('development')
+            }
         }),
-        new ExtractTextPlugin('styles.css'),
-        new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendor', 'manifest'],
-            minChunks: Infinity,
-        }),
-        new webpack.HashedModuleIdsPlugin(),
-        new WebpackChunkHash(),
-        new ChunkManifestPlugin({
-            filename: "chunk-manifest.json",
-            manifestVariable: "webpackManifest"
-        }),
-        function() {
-            this.plugin("done", function(stats) {
-                fs.writeFileSync(
-                    path.join(__dirname, build, "stats.json"),
-                    JSON.stringify(stats.toJson()));
-            });
-        }
+        new StyleLintPlugin()
     ]
-};
+});
