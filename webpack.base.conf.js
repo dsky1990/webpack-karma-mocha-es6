5b778974c8509c72422764f4c85704c0fb6373ef
dsky1990 <dutian1990@gmail.com>
2017-03-20 20:31:26 +0800

    update base config

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,7 +1,61 @@
 /**
  * Created by dsky on 2017/3/17.
  */
-// base config
+
+// get filePath
+const fs = require('fs'),
+    path = require('path'),
+    StyleLintPlugin = require('stylelint-webpack-plugin');;
+
+//get floder path
+const filePath = JSON.parse(fs.readFileSync('./file.json', 'utf8'));
+const buildSass = filePath.build.sass,
+    buildCss = filePath.build.css,
+    buildEs6 = filePath.build.es6,
+    buildJs = filePath.build.js,
+    buildIcon = filePath.build.icon,
+    buildImg = filePath.build.img,
+    distCss = filePath.dist.css,
+    distImg = filePath.dist.img,
+    distJs = filePath.dist.js;
+
+// mkdir fun
+const mkdirSync = function (path) {
+    if(path){
+        try {
+            fs.mkdirSync(path);
+        } catch(e) {
+            if ( e.code != 'EEXIST' ) throw e;
+        }
+    }
+};
+
+const mkdirpSync = function (dirpath) {
+    if(dirpath){
+        const parts = dirpath.split(path.sep);
+        const len = parts.length;
+        for( let i = 1; i <= len ; i++ ) {
+            mkdirSync( path.join.apply(null, parts.slice(0, i)) );
+        }
+    }
+};
+// creat floder
+mkdirpSync(buildSass);
+mkdirpSync(buildCss);
+mkdirpSync(buildEs6);
+mkdirpSync(buildJs);
+mkdirpSync(buildIcon);
+mkdirpSync(buildImg);
+mkdirpSync(distCss);
+mkdirpSync(distImg);
+mkdirpSync(distJs);
+
+/*
+* base config
+* base rules and plugin
+* load css
+*
+*/
 module.exports = {
     entry: {
         index: '',
@@ -12,27 +66,33 @@ module.exports = {
         path: path.reslove(_dirname, build)
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                use: 'css-loader'
-            })
-        },{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: [['es2015', {modules: false}]],
-                    plugins: [
-                        'syntax-dynamic-import',
-                        'transform-async-to-generator',
-                        'transform-regenerator',
-                        'transform-runtime'
-                    ]
-                }
-            }]
-        }]
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: 'inline',
+                            plugins: function () {
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
