04184626f1d75c4a17785b7803f85a2560d041d9
dsky1990 <dutian1990@gmail.com>
2017-03-22 21:33:19 +0800

    update config file

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
 /**
  * Created by dsky on 2017/3/20.
  */
-
+module.exports = {
+    NODE_ENV: '"production"'
+};
--- config/test.js
+++ config/test.js
@@ -1,3 +1,6 @@
 /**
  * Created by dsky on 2017/3/22.
  */
+module.exports = {
+    NODE_ENV: '"testing"'
+};
